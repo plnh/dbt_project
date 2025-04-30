f42f968a0ab765e5209a849f2bd0fe9fe6c6f8cf
Ha Pham <ha.phamly@outlook.com>
2025-04-30 14:14:17 +0000

    correct schema in asset.sql

--- models/marts/asset.sql
+++ models/marts/asset.sql
@@ -1,4 +1,4 @@
-{%- set inv_models = dbt_utils.get_relations_by_pattern(schema_pattern='stg', table_pattern='%_inv') %}
+{%- set inv_models = dbt_utils.get_relations_by_pattern(schema_pattern='dbt_hpham', table_pattern='%_inv') %}
 
 {%- if inv_models | count == 0 %}
     select null as dummy
