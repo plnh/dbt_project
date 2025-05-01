7036c465815766dc71620872b942f3688bcb6e83
Ha Pham <ha.phamly@outlook.com>
2025-05-01 13:14:11 +0000

    fixed bugs

--- macros/utl/cast_to_date.sql
+++ macros/utl/cast_to_date.sql
@@ -1,10 +1,10 @@
 {% macro cast_to_date(column_name)%}
     coalesce(
-        PARSE_DATE('%Y-%m-%d', {{ column_name }}),
-        PARSE_DATE('%m/%d/%Y', {{ column_name }}),
-        PARSE_DATE('%d/%m/%Y', {{ column_name }}),
-        PARSE_DATE('%Y/%m/%d', {{ column_name }}),
-        PARSE_DATE('%d-%m-%Y', {{ column_name }}),
-        PARSE_DATE('%m-%d-%Y', {{ column_name }})
+        SAFE.PARSE_DATE('%Y-%m-%d', {{ column_name }}),
+        SAFE.PARSE_DATE('%m/%d/%Y', {{ column_name }}),
+        SAFE.PARSE_DATE('%d/%m/%Y', {{ column_name }}),
+        SAFE.PARSE_DATE('%Y/%m/%d', {{ column_name }}),
+        SAFE.PARSE_DATE('%d-%m-%Y', {{ column_name }}),
+        SAFE.PARSE_DATE('%m-%d-%Y', {{ column_name }})
     )
 {% endmacro %}
--- models/intermediate/int_metal_inv.sql
+++ models/intermediate/int_metal_inv.sql
@@ -1,23 +1,27 @@
+{% set number_columns = ['weight_gram', 'purchase_price', 'net_value', 'fee', 'total_cost'] %}
+
 with
-staging as (
-
-    select * from {{ ref('stg_metal_inv') }}
-
-),
-    transformed as (
-       select 
+transformed as (
+select 
     asset_type ,
     type, 
     asset_name,
     unit_type,
-    cast(weight_gram as FLOAT64)  as weight_gram,
-    cast(unit_owned as INT64 )as unit_owned,
-    purchase_date,
-    cast(purchase_price as FLOAT64) as purchase_price,
-    cast(net_value as FLOAT64) as net_value,
-    cast(fee as FLOAT64) as fee,
-    cast(total_cost as FLOAT64) as total_cost
+    {{- cast_to_date('purchase_date') -}} as purchase_date,
+    {{- cast_to_number('unit_owned', 'INT64') -}} as unit_owned,
+    case when unit_type like '%Coin%' then 0.9 else 1 end as purity,
+    {%- for col in number_columns -%}
+        {{ cast_to_number(col)}} as {{ col -}} ,
+    {%- endfor %}
+        
+   from  {{ ref('stg_metal_inv') }}
+)
+, 
+computed as 
+(
+    select *,
+        (weight_gram*purity)*unit_owned as total_weight_gram
+    from transformed
+)
 
-from staging
-
-select * from transformed
+select * from computed
