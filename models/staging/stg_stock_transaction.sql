4913fa03edcee33768718c9f42160fecc6c3980c
Ha Pham <ha.phamly@outlook.com>
2025-07-31 14:49:16 +0000

    add total cost to stg_stock_transaction

--- models/staging/stg_stock_transaction.sql
+++ models/staging/stg_stock_transaction.sql
@@ -19,22 +19,18 @@ renamed as (
             Commission_in_EUR *(-1) as fee,
             Exchange_Rate as FX
         from source
-    )
-
+    ),
+transformed as (
 select
-    asset_type,
-    purchase_date	,
-    asset_name ,
-    transaction_type,
-    ticker,
-    unit_owned,
-    local_currency ,
-    FX,
+    *,
     CASE WHEN local_currency LIKE 'EUR' THEN purchase_price_lc
         ELSE purchase_price_lc END AS purchase_price,
-    fee ,
-    purchase_price_lc,
     CASE WHEN local_currency not LIKE 'EUR' THEN fee/FX
         ELSE fee END AS fee_lc
 
-from renamed
+from renamed)
+
+select * ,
+purchase_price_lc + fee_lc as total_cost_lc,
+purchase_price + fee as total_cost
+from transformed
