bdb02ceb0a9a268617ae40728f05906279154548
Ha Pham <ha.phamly@outlook.com>
2025-07-31 13:22:01 +0000

    update stg_stock_inv

--- models/staging/stg_stock_inv.sql
+++ models/staging/stg_stock_inv.sql
@@ -3,32 +3,38 @@ source as
 (
     select *
     from {{ source('MyDoc', 'Stock Inventory') }}
-    where string_field_0 is not null and string_field_0 not like 'Asset Name'
 
 ),
 
 renamed as (
-        {%- set column_name = [
-            "asset_name",
-            "ticker",
-            "type",
-            "sector",
-            "purchase_date",
-            "unit_owned",
-            "purchase_price",
-            "net_value",
-            "fee",
-            "total_cost",
-        ] -%}
-
         select
             "financial_assets" as asset_type,
-            {%- for i in range(10) %}
-                string_field_{{ i }} as {{ column_name[i] }}
-                {%- if not loop.last -%}, {%- endif -%}
-            {%- endfor %}
+            'Date' as purchase_date	,
+            Description	as asset_name ,
+            Transaction_Type as transaction_type,
+            Symbol	as ticker,
+            Quantity as unit_owned,
+            CAST( SPLIT(Price_currency, ' ')[OFFSET(0)] AS NUMERIC) as purchase_price_lc,
+            SPLIT(Price_currency, ' ')[SAFE_OFFSET(2)] AS local_currency,
+            Commission_in_EUR *(-1) as fee,
+            Exchange_Rate as FX
         from source
     )
 
-select *
+select
+    asset_type,
+    purchase_date	,
+    asset_name ,
+    transaction_type,
+    ticker,
+    unit_owned,
+    local_currency ,
+    FX,
+    CASE WHEN local_currency LIKE 'EUR' THEN purchase_price_lc
+        ELSE purchase_price_lc END AS purchase_price,
+    fee ,
+    purchase_price_lc,
+    CASE WHEN local_currency not LIKE 'EUR' THEN fee/FX
+        ELSE fee END AS fee_lc
+
 from renamed
