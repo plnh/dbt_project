a572900591f5b1afa2927406f2718112645d74be
Ha Pham <ha.phamly@outlook.com>
2025-08-01 13:16:03 +0000

    fix bug

--- models/intermediate/int_stock_price.sql
+++ models/intermediate/int_stock_price.sql
@@ -1,24 +1,21 @@
 with 
-filter_transform as 
+source as 
 (
     select  
         update_date
         ,{{- cast_to_number("price") }} as price
-        ,price_profile
-        ,type
-    from {{ref("stg_gold_price")}}
-    where price_profile like "prime"
+        ,ticker
+    from {{ref("stg_stock_price")}}
 )
 , normalize as
 (
     SELECT
         DATE_TRUNC(update_date, MONTH) AS update_date,
-        type, 
-        AVG(price) AS average_price,
-        AVG(price/31.1) AS avg_price_per_gram
-    FROM filter_transform
+        ticker, 
+        Round(AVG(price),2) AS average_price
+    FROM source
     GROUP BY 1,2
-    ORDER BY 1
+    ORDER BY 1, 2
 )
 
 select * 
