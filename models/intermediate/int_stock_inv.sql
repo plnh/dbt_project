9ada3e6992d44f0984bfb16d6e8f77962190d83c
Ha Pham <ha.phamly@outlook.com>
2025-08-01 13:03:39 +0000

    update

--- models/intermediate/int_stock_inv.sql
+++ models/intermediate/int_stock_inv.sql
@@ -1,19 +1,35 @@
-{% set columns = [
-    "purchase_price",
-    "purchase_price_lc",
-    "fee",
-    "fee_lc",
-    "total_cost",
-    "total_lc"
-] %}
 
-select
-asset_type, 
-ticker, 
-unit_owned,
-{%- for col in columns -%}
-    {{ convert_to_zero(col) }} ,
-{%- endfor %}
-date_trunc(purchase_date, month) as purchase_month
-        from {{ ref("stg_stock_transaction") }}
-from {{ ref("stg_stock_transaction") }}
+with 
+transaction as (
+    select
+    asset_type,
+    ticker, 
+    DATE_TRUNC(purchase_date, MONTH) AS purchase_month,
+    sum(unit_owned) as unit_owned
+
+    from {{ ref("stg_stock_transaction") }}
+    group by 1,2,3
+    )
+
+,dim_date as (
+    select date_month, year_month from {{ ref("dim_date") }}
+    )
+, ticker_date as (
+    select d.* , asset_type, ticker
+    from dim_date d
+    cross join (select asset_type, ticker, min(purchase_month) as min_date from transaction group by 1,2 ) x 
+    where d.date_month >= min_date
+    order by ticker, date_month
+)
+, fact_table as (
+    select
+        d.date_month, d.year_month, d.asset_type, d.ticker,  unit_owned,
+        sum(unit_owned) over (partition by d.asset_type, d.ticker order by  d.date_month) as cumulative_units
+    from transaction t 
+    right join ticker_date d on  t.ticker = d.ticker and t.purchase_month = d.date_month
+    order by d.asset_type, d.ticker, d.date_month
+    )
+
+select * from   fact_table
+ 
+ 
