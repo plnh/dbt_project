413371303f4e625d8c2fe9cccff843ccdf89b45b
Ha Pham <ha.phamly@outlook.com>
2025-08-04 15:05:10 +0000

    update

---
+++ models/marts/asset_value.sql
@@ -0,0 +1,26 @@
+with
+    metal_assets as (
+        select
+            date_month,
+            year_month,
+            asset_type,
+            asset_desc,
+            sum(cumulative_value) as cumulative_value
+        from {{ ref("asset_metal_cost") }}
+        group by 1,2,3,4
+    ),
+
+    stock_assets as (
+        select
+            date_month,
+            year_month,
+            asset_type,
+            asset_desc,
+            local_currency,
+            sum(cumulative_value) as cumulative_value
+        from {{ ref("asset_stock_value") }}
+        group by 1,2,3,4,5
+    )
+
+select * 
+from stock_assets
